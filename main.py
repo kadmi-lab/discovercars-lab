0125e73c7ef8894f31c01c6b6b0a4ac3222340c3
Maksims Sirovatskis <maksims.sirovatskis@gmail.com>
2023-04-17 18:54:46 +0300

    Finish subtask 1.2.1

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import os
+import json
 from flask import Flask, make_response
+from flask_restful import Api
+from simplexml import dumps
+
 app = Flask(__name__)
 api = Api(app)
+
+@api.representation('application/xml')
+def output_xml(data, code, headers=None):
+  resp = make_response(dumps({'response' : data}), code)
+  resp.headers.extend(headers or {})
+  return resp
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+  resp = make_response(json.dumps({'response' : data}), code)
+  resp.headers.extend(headers or {})
+  return resp
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
