6b16a2833b919edaef8ed7757468a7e1bac48d84
Maksims Sirovatskis <maksims.sirovatskis@gmail.com>
2023-04-17 18:54:13 +0300

    Finish subtask 1.2

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask, make_response
 app = Flask(__name__)
 api = Api(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/environment")
+def environment():
+    return '<br>'.join(os.environ)
